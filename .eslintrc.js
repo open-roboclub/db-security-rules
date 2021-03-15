5396db0bcb0b23b324647e12eb59e1154f15ef29
Areeb Jamal <jamal.areeb@gmail.com>
2021-03-15 19:53:26 +0530

    chore: Refactor project with linters

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    mocha: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+};
